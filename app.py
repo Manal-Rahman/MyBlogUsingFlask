443900cf4e30582168909858e392a558e782d4a8
Manal-Rahman <manalrahman5890@gmail.com>
2024-12-29 14:58:41 +0500

    Made Contact page with Database (sqlAlchemy)

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template,request,session
+from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
 
 
 
@@ -12,14 +13,45 @@ with open('config.json','r') as c:
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/blog"
+
+db = SQLAlchemy(app)
+
+class Contact(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(120),primary_key=False,nullable=False,unique=False)
+    email=db.Column(db.String(120),primary_key=False,nullable=False,unique=True)
+    phone_num=db.Column(db.String(120),primary_key=False,nullable=False,unique=True)
+    message=db.Column(db.String(120),primary_key=False,nullable=False,unique=False)
+
+
+
+   
+
+
 
 @app.route("/")
 def home():
     return render_template("index.html",params=params)
 
 
-@app.route("/contact")
+@app.route("/contact",methods=["GET","POST"])
 def contact():
+    if(request.method=="POST"):
+        name=request.form.get("name")
+        email=request.form.get("email")
+        phone_num=request.form.get("phone_num")
+        message=request.form.get("message")
+        entry = Contact(name=name,email=email,phone_num=phone_num,message=message)
+        db.session.add(entry)
+        db.session.commit()
+
+
+
+
+
+
+
     return render_template("contact.html",params=params)
 
 
